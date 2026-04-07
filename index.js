6d32767d710782eac6479a136aa8aee28bb3839e
Leons20 <95307550+Leons20@users.noreply.github.com>
2026-04-07 19:00:37 +0200

    Added larger limit for profile images

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 let db = await connectToDatabase();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 app.use((req, res, next) => {
